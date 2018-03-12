a4517b0c83a02c1a3306f11c9688b53f6d84ab1c
bijoykumar.mohanta <bijoy.cse09@gmail.com>
2018-03-12 14:38:53 +0600

    2nd commit

--- TestProject/src/package1/StartUp.java
+++ TestProject/src/package1/StartUp.java
@@ -5,7 +5,11 @@ public class StartUp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		Sum(50,50);
 
 	}
 
+	private static void Sum(int a, int b) {
+		System.out.println(a+b);
+	}
 }
